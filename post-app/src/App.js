1f24a3ea0d369a424885d2d4ca29a44aa7277a48
ISSAM1441 <issambourne@gmail.com>
2022-05-23 07:08:12 +0100

    Integration page routing & project structure rev2

--- post-app/src/App.js
+++ post-app/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import PostsReview from './component/PostsReview';
+import PostDetails from './component/PostDetails';
+import ErrorPage from './component/ErrorPage';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/*============================= Main page Posts =============================*/}
+          <Route path="/" exact element={<PostsReview />} />
+
+          {/*========================= Detailed post page by Id ========================*/}
+          <Route path="/post/:postId" element={<PostDetails />} />
+
+          {/*========================== Error Page not found ===========================*/}
+          <Route path="*" element={<ErrorPage />} />
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
